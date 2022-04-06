cc23b4755d587a69a428205f3c6351dce9cf0dd5
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-04-06 00:37:34 -0400

    App up and running, all AC meet, missing comments

---
+++ README.md
@@ -0,0 +1,17 @@
+# Work Day Scheduler
+
+## Links
+1. Repository: https://github.com/rrangel07/5-Day_Planner
+2. Implementation: https://rrangel07.github.io/5-Day_Planner/
+
+## Description
+
+When the user access the site, they are presented a table with divisions for the time of the date and 15 min intervals subdivisions so they can fit in it the activities that need to remember during the day.
+
+On the top of the page, the user will be able to see a clock with the current day, date and time.
+
+When the time has passed and it's later than one of the time blocks, this time block's background will change color to let the user know that time has already passed.
+
+Red for past events, yellow for current events and green for upcoming ones. The user has multiple save buttons that save all the timeblocks in local memory and also multiple erase buttons that delete only the timeblock they're embeded in.
+
+![Work Day Scheduler](./assets/images/Work%20Day%20Scheduler.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -33,16 +33,21 @@ var time=
 var $timeCards=[];
 var $timeSubdivisions=[];
 var $timeContainers=[];
+var $saveButtons;
 var intervals;
+var currentTime;
 
 console.log(time);
+$headerEl.text(moment().format("dddd, MMMM Do YYYY, HH:mm:ss"));
 function createCards(){
+    currentTime= moment().format('HH')
+    console.log(currentTime);
     for(let i=0; i<time.length;i++){
         $contEl.append(
         `<div class="card-group flex-row justify-content-between" id="group${i}">
             <div class="card col-sm-1 col-1">
                 <div class="card-body d-flex align-items-center justify-content-center">
-                    <p class="card-text" id="time-${i}">${time[i].time}</p>
+                    <p class="card-text" id="time-${i}">${moment(time[i].time,'HH').format('HH')}</p>
                 </div>
             </div>
             <div class="card col-10">
@@ -51,8 +56,9 @@ function createCards(){
                 </div>
             </div>
             <div class="card col-1 col-sm-1">
-                <div class="card-body">
-                    <p class="card-text"></p>
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <button class="save-button" tittle='click to save'><i class="fa-solid fa-floppy-disk save-button" style="font-size:30px" ></i></button>
+                    <button class="delete-button"><i class="fa-solid fa-trash delete-button" style="font-size:30px"></i></button>
                 </div>
             </div>
         </div>`);
@@ -63,24 +69,71 @@ function createCards(){
         $timeContainers.push($(`#time-${i}`));
         console.log($timeContainers);
         for(let j=0; j<4; j++){
-            // intervals= moment(moment.duration(j*15, 'minutes')).format('mm');
-            intervals= moment.duration({minutes: j*15});
-            intervals= intervals.format('mm', { trim: false })
+            intervals= moment(j*15,'mm').format('mm');
             console.log(intervals);
             $timeSubdivisions[i].append(
                 `<div class="d-flex align-items-center mw-100">
-                    <p class="mx-3 mb-0 py-2px">:${intervals}</p><textarea class="card-text" placeholder="Set your event here"></textarea>
+                    <p class="mx-3 mb-0 py-2px">:${intervals}</p><textarea class="card-text" placeholder="Set your event here" id='text-${$timeContainers[i].text()}-${j}'></textarea>
                 </div>
                 <hr>`
             );
         }
+        if (moment(time[i].time,'HH').format('HH') < currentTime){
+            $timeCards[i].addClass('past-time');
+        } else if(moment(time[i].time,'HH').format('HH') > currentTime){
+            $timeCards[i].addClass('future-time');
+        } else{
+            $timeCards[i].addClass('present-time');
+        }
+        
     }
 }
+function setEventListeners(){
+    $contEl.on('click', saveDelete);
+}
 
-createCards();
-// moment().format('[j*15]')
-// moment({minute:})
+function saveDelete(evt){
+        var target= $(evt.target);
+        console.log(target);
+        console.log(target.hasClass('save-button'));
+        if (target.hasClass('save-button')){
+            storeTasks();
+        }else if (target.hasClass('delete-button')){
+            var trans= (((target.parents("div[id^='group']")).children().eq(1)).children()).children('div');
+            var textEl;
+            for (let i=0;i<4;i++){
+                textEl= ((trans.eq(i)).children('textarea'));
+                textEl.val('');
+            }
+            storeTasks();
+        }
+}
 
-// moment(momento._data.minutes).format('mm');
-// '00'
+function storeTasks(){
+    for (let i=0;i<time.length;i++){
+        for (let j=0;j<4;j++){
+            time[i].tasks[j]=($(`#text-${$timeContainers[i].text()}-${j}`)).val();
+        }
+    }
+    localStorage.setItem('savedTasks',JSON.stringify(time));
+}
+function retrieveTasks(){
+   var tempTasks=JSON.parse(localStorage.getItem('savedTasks'));
+   if (tempTasks !== null){
+       time=tempTasks;
+       for (let i=0;i<time.length;i++){
+        for (let j=0;j<4;j++){
+            ($(`#text-${$timeContainers[i].text()}-${j}`)).val(`${time[i].tasks[j]}`);
+        }
+    }
+   }
+    
+}
 
+function init(){
+    setEventListeners();
+    createCards();
+    retrieveTasks();
+}
+
+init();
